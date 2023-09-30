6a18dc8aaddd5ff200decfd444a5b31d7a04c5c0
20820@Aditya <2082001aditya@gmail.com>
2023-09-30 06:57:56 +0530

    created mysql connection and executed mysql query

---
+++ backend/models/db.js
@@ -0,0 +1,10 @@
+import mysql from 'mysql2/promise';
+import {config} from "../config.js"
+
+const  query = async (sql, params) =>{
+  const connection = await mysql.createConnection(config.db);
+  const [results, ] = await connection.execute(sql, params);
+  return results;
+}
+
+export default query;
