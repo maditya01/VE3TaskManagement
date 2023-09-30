9e1e38ec39542ca7b9f2a06b4c615a9b758bf908
20820@Aditya <2082001aditya@gmail.com>
2023-09-30 10:27:33 +0530

    added home and auth routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './components/Home/Home';
+import { Auth } from './components/Auth/Auth';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/auth" element={<Auth />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
