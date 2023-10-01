65ea525abea78f59d039dc681aa7f6dfdf8a6d49
20820@Aditya <2082001aditya@gmail.com>
2023-10-01 07:00:42 +0530

    get all task from backend

--- frontend/src/components/Tasks/Tasks.jsx
+++ frontend/src/components/Tasks/Tasks.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Task from './Task/Task.jsx';
+import axios from 'axios';
 
-const Tasks = () => {
-    
+const Tasks = ({taskList}) => {
+  const [allTasks, setAllTasks] = useState([]);
+  const fetchAllTasks = async () => {
+    const { data } = await axios.get("http://localhost:3001/tasks");
+    setAllTasks(data.data);
+    console.log(data.data);
+  }
+  useEffect(() => {
+    fetchAllTasks();
+  }, [taskList])
+  console.log(allTasks?.length);
+  if (!allTasks?.length) {
+    return 'No Tasks'
+  }
   return (
-    <div>Tasks</div>
+    <>
+      {allTasks?.map((task) => (<Task task={task} />))}
+    </>
   )
 }
 
