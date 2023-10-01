67dfd3b58d2a32069e51982a817b9fed0e78d577
20820@Aditya <2082001aditya@gmail.com>
2023-10-01 11:02:12 +0530

    sign up and sign in routes created

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,9 @@
 import express from 'express';
-
+import { signinUser, signupUser } from '../controllers/user.js';
 const router = express.Router();
+
+router.post('/signup', signupUser)
+router.post('/signIn', signinUser);
+
 
 export default router;
