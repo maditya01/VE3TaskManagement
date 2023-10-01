c25e8d884cfd1d70c2cc40d59e35292fc8b81f9a
20820@Aditya <2082001aditya@gmail.com>
2023-10-01 07:01:36 +0530

    passed props to Tasks and Form Component

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Tasks from '../Tasks/Tasks'
 import Form from '../Form/Form'
 
 const Home = () => {
+    const [taskList, setTaskList] = useState(null);
     return (
-        <div className="text-white bg-purple-800 p-5 m-5 flex flex-wrap justify-around">
+        <div className="text-white  p-5 m-5 flex flex-wrap justify-around">
             <div className ="flex flex-wrap flex-col">
                 <h1 className="text-3xl font-bold"> Your Task </h1>
-                <Tasks />
+                <Tasks taskList={taskList} />
             </div>
             <div className = "flex flex-wrap flex-col">
                 <div>
                 <h1 className='text-3xl font-bold'> Create New Task</h1>
                 </div>
                 <div>
-                  <Form />
+                  <Form setTaskList={setTaskList} />
                 </div>
             </div>
         </div>
