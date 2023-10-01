d8bbcb008fd7c3185b56f8c9987c0d706b57043e
20820@Aditya <2082001aditya@gmail.com>
2023-10-01 13:21:22 +0530

    route privacy added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Route, Routes,Navigate } from 'react-router-dom'
 import Home from './components/Home/Home';
 import { useState } from 'react';
 import { Auth } from './components/Auth/Auth';
@@ -7,13 +7,13 @@ import Form from './components/Form/Form';
 
 function App() {
   const [taskList, setTaskList] = useState(null);
+  const [user,setUser] = useState(localStorage.getItem('profile'))
   return (
     <BrowserRouter>
-      
       <Routes>
-        <Route path="/" element={<Home taskList={taskList} />}></Route>
-        <Route path="/createtask" element={<Form setTaskList={setTaskList} />}></Route>
-        <Route path="/auth" element={<Auth />}></Route>
+        <Route path="/" element={user ? <Home taskList={taskList} setUser={setUser}/> : <Navigate to ="/auth" />}></Route>
+        {user && <Route path="/createtask" element={<Form setTaskList={setTaskList} />}></Route>}
+        <Route path="/auth" element={<Auth setUser={setUser} />}></Route>
       </Routes>
     </BrowserRouter>
   );
