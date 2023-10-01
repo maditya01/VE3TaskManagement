d9bbb6c569fe586f963bb5f379aa53a577ebec78
20820@Aditya <2082001aditya@gmail.com>
2023-10-01 11:04:26 +0530

    createtask route added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,18 @@
 import './App.css';
-import Navbar from './components/Navbar/Navbar';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './components/Home/Home';
+import { useState } from 'react';
 import { Auth } from './components/Auth/Auth';
+import Form from './components/Form/Form';
 
 function App() {
+  const [taskList, setTaskList] = useState(null);
   return (
     <BrowserRouter>
-      <Navbar />
+      
       <Routes>
-        <Route path="/" element={<Home />}></Route>
+        <Route path="/" element={<Home taskList={taskList} />}></Route>
+        <Route path="/createtask" element={<Form setTaskList={setTaskList} />}></Route>
         <Route path="/auth" element={<Auth />}></Route>
       </Routes>
     </BrowserRouter>
