5e33ccc59bca5f3d384be0fae11148941f5b000b
20820@Aditya <2082001aditya@gmail.com>
2023-10-01 07:02:48 +0530

    write the sql query for creating task

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -1,53 +1,57 @@
 
 import query from '../models/db.js'
 
-export const fetchAllTasks = async (req,res)=>{
+export const fetchAllTasks = async (req, res) => {
     try {
         const rows = await query(
             `SELECT taskId, taskCreator, taskTitle, taskMessage from tasks`
         );
-        res.status(200).json({data:rows});
+        res.status(200).json({ data: rows });
     } catch (error) {
         console.log(error);
     }
 }
 
-export const fetchSingleTask = async (req,res)=>{
-    const { id }  = req.params;
+export const fetchSingleTask = async (req, res) => {
+    const { id } = req.params;
     try {
         const row = await query(
             `select taskId,taskCreator,taskTitle,taskMessage from tasks where taskId = ${id}`
         );
         console.log(row);
-        res.status(200).json({data:row});
+        res.status(200).json({ data: row });
     } catch (error) {
-        
+
     }
 }
 
-export const updateTask = async (req,res)=>{
-    const { id }  = req.params;
+export const updateTask = async (req, res) => {
+    const { id } = req.params;
     try {
-        
+
     } catch (error) {
-        
+
     }
 }
 
-export const createTask = async (req,res)=>{
+export const createTask = async (req, res) => {
     const task = req.body;
+    console.log(task);
     try {
-       query(`INSERT INTO tasks `) 
+        const row = await query(`INSERT INTO tasks (taskCreator, taskTitle, taskMessage) values ('${task.creator}', '${task.title}', '${task.message}')`);
+        if (row.affectedRows) {
+            res.status(200).json({ message: "succesfully inserted" });
+        }
     } catch (error) {
-        
+        console.log(error);
     }
 }
 
-export const deleteTask = async (req,res)=>{
-    const {_id}  = req.params;
+export const deleteTask = async (req, res) => {
+    const { _id } = req.params;
     try {
-        
+
     } catch (error) {
-        
+
     }
 }
