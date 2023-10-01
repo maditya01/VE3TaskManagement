78cdbbf93da2fa4bd3746965e4fb85d5ad6e4c85
20820@Aditya <2082001aditya@gmail.com>
2023-10-01 13:20:01 +0530

    signinUser controller function

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -11,4 +11,20 @@ export const signupUser = async (req, res) => {
         console.log(error);
     }
 }
-export const signinUser = () => { }
+export const signinUser = async (req, res) => {
+    const details = req.body;
+    console.log(details);
+    try {
+        const row = await query(`SELECT useremail,userpassword from users where useremail = '${details.useremail}' `)
+        console.log(row.length);
+        if (row.length > 0 && details.userpassword === row[0].userpassword) {
+            console.log("same password!!!")
+            res.status(200).json({ message: 'successfully authenticated!!!' });
+        } else {
+            res.status(409).json({ message: 'Login failure please write correct data' })
+        }
+    } catch (error) {
+        console.log(error);
+    }
+
+}
