daeb2053489006b7b243665a63abf7615eec92ec
20820@Aditya <2082001aditya@gmail.com>
2023-10-02 10:45:46 +0530

    single task page added delete and update icon

--- frontend/src/components/Tasks/Task/Task.jsx
+++ frontend/src/components/Tasks/Task/Task.jsx
@@ -1,13 +1,50 @@
 import React from 'react'
-
-const Task = ({ task }) => {
-  // console.log("come")
+import Icon from "react-crud-icons";
+import axios from 'axios';
+import "../../../../node_modules/react-crud-icons/dist/css/react-crud-icons.css";
+import { useNavigate } from 'react-router-dom';
+const Task = ({ setUpdate, task, setTaskId, allTasks, setAllTasks }) => {
+  const navigate = useNavigate();
+  const iconClick = () => {
+    navigate(`/updatetask/${task.taskId}`);
+  }
+  const deleteClick = async () => {
+    axios.delete(`http://localhost:3001/tasks/${task.taskId}`).
+      then((response) => {
+        const remaintask = allTasks.filter((mtask) => mtask.taskId !== task.taskId)
+        console.log(remaintask);
+        setAllTasks(remaintask);
+        console.log(response);
+      }).
+      catch((error) => {
+        console.log(error);
+      });
+  }
   return (
     <>
-      <div className = "bg-red-100 m-4 flex flex-col p-2 w-26">
-        <p>{task.taskId}</p>
-        <div>{task.taskTitle}</div>
-        <div>{task.taskMessage}</div>
+      <div className="bg-red-100 m-4  p-4 w-26">
+        <Icon
+          name="edit"
+          tooltip="Edit"
+          theme="light"
+          size="small"
+          onClick={iconClick}
+        />
+        <Icon
+          name="delete"
+          tooltip="Delete"
+          theme="light"
+          size="small"
+          onClick={deleteClick}
+        />
+        <div className='flex justify-between'>
+          <h1 className="text-xl font-bold ">Title - </h1>
+          <div className="bg-yellow-500">{task.taskTitle}</div>
+        </div>
+        <div className='flex'>
+          <h1 className="text-xl font-bold">Message- </h1>
+          <div className="w-full bg-red-800">{task.taskMessage}</div>
+        </div>
       </div>
     </>
   )
