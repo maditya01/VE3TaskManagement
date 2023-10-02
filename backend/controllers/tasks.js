45e02706181098ade5f38e86390ce076452dd96a
20820@Aditya <2082001aditya@gmail.com>
2023-10-02 18:23:26 +0530

    send response for error message

--- backend/controllers/tasks.js
+++ backend/controllers/tasks.js
@@ -20,7 +20,8 @@ export const fetchSingleTask = async (req, res) => {
             `select taskId,taskCreator,taskTitle,taskMessage from tasks where taskId = ${id}`
         );
         console.log(row);
-        res.status(200).json({ data: row });
+        if (row.length > 0) res.status(200).json({ data: row });
+        else res.status(404).json({ message: "Given task does not exist" })
     } catch (error) {
         console.log(error)
     }
@@ -41,14 +42,21 @@ export const updateTask = async (req, res) => {
 }
 
 export const createTask = async (req, res) => {
-    const task = req.body;
+    let task = req.body;
+    let mess = task.message;
+    let tit = task.title;
+    mess = mess.replace(/'/g, '"')
+    tit = tit.replace(/'/g, '"')
     try {
-        const row = await query(`INSERT INTO tasks (taskCreator, taskTitle, taskMessage) values ('${task.creator}', '${task.title}', '${task.message}')`);
+        const row = await query(`INSERT INTO tasks (taskCreator, taskTitle, taskMessage) values ('${task.creator}', '${tit}', '${mess}')`);
         if (row.affectedRows) {
             res.status(200).json({ message: "succesfully inserted" });
+        } else {
+            res.status(404).json({ message: "Given data has not been inserted" })
         }
     } catch (error) {
         console.log(error);
+        res.status(400).json({ message: "Given data has not been inserted" })
     }
 }
 
@@ -58,7 +66,7 @@ export const deleteTask = async (req, res) => {
         const postRow = await query(`delete from tasks where taskId='${id}'`)
         if (postRow.affectedRows) {
             res.status(200).json({ message: `${id} is deleted` });
-        }
+        } else res.status(404).json({ message: "Hun not so smart does not exist " })
     } catch (error) {
         console.log(error);
     }
